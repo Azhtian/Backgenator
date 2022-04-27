2a2c14cebde2f0521b8472cbb6426dbc9aaea34c
Stian Rykkje <sry019@uib.no>
2022-04-27 14:26:18 +0200

    cleaned up project

--- components/Center.js
+++ components/Center.js
@@ -1,50 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../styles/Home.module.css'
 
-var color1 = "#222222";
-var color2 = "#ffffff";
-var rotation = "45deg";
-var current = "h3";
-var textContent = `linear-gradient(90deg, ${color1}, ${color2})`;
-//color1.addEventListener("input", setColor);
-//color2.addEventListener("input", setColor);
-
-
-function setColor() {
-    bg.style.background = `linear-gradient(90deg, ${color1.value}, ${color2.value})`
-    current.textContent = bg.style.background + ";";
-}
 
 class Center extends React.Component {
+
     constructor(props) {
         super(props);
-        this.color1 = '666000';
-        this.color2 = 'ffffff';
-        this.rotation = {value: '45deg'};
-
+        this.color1 = {name: 'picker1', value: '#000000'};
+        this.color2 = {name: 'picker2', value: '#ffffff'};
+        this.rotation = 90;
+        this.textContent = `linear-gradient(${this.rotation + 'deg'}, ${this.color1.value}, ${this.color2.value})`
         this.handleChange = this.handleChange.bind(this);
     }
     
     handleChange(event) {
+        const { name, value } = event.target;
         this.setState({value: event.target.value});
-        this.color1 = event.target.value;
-        this.color2 = event.target.value;
-      }
+        if (name == this.color1.name)
+            this.color1.value = value;
+        if (name == this.color2.name)
+            this.color2.value = value;
+        if (name == "range")
+            this.rotation = value;
+        this.textContent = `linear-gradient(${this.rotation + 'deg'}, ${this.color1.value}, ${this.color2.value})`
+    }
     
     render() {
         return (
             <main className={styles.main} 
                 style={{
-                    background:`linear-gradient(${this.rotation}, ${this.color1.value}, ${this.color2.value})`
+                    background:`linear-gradient(${this.rotation + 'deg'}, ${this.color1.value}, ${this.color2.value})`
                 }}>
-                <div className={styles.card}>
+                <div className={styles.card} >
                     <h1>Background Gradient Generator</h1>
-                    <div id="pickers">
-                        <input type="color" name="" id="color1" value={this.color1.value} onChange={this.handleChange}></input>
-                        <input type="color" name="" id="color2" value={this.color2.value} onChange={this.handleChange}></input>
+                    <div className={styles.pickers} >
+                        <input type="color" name="picker1" id="color1" value={this.color1.value} onChange={this.handleChange}></input>
+                        <input type="color" name="picker2" id="color2" value={this.color2.value} onChange={this.handleChange}></input>
+                    </div>
+                    <div>
+                        <input className={styles.slider} type="range" name="range" id="rotation" min={"0"} max={"360"} value={this.rotation} onChange={this.handleChange}></input>
                     </div>
                     <h2>Current CSS Background</h2>
-                    <h3>{textContent}</h3>
+                    <h3>{this.textContent}</h3>
                 </div>
             </main>
         )
